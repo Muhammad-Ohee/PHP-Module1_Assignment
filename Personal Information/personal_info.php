5165d51d6e86c896787a9a9b6f997d45935741df
Muhammad Ohee <md.ohee786@gmail.com>
2023-09-12 12:26:44 +0600

    version 2

---
+++ Personal Information/personal_info.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Personal Information</title>
+    </head>
+    <body>
+        <?php
+            $name = "Muhammad Makama Mahmudur Rahman Ohee";
+            $age = 29;
+            $country = "Bangladesh";
+            $about_myself = "💙 Hello, I am Ohee. I have graduated from Notre Dame University Bangladesh in Computer Science and Engineering department. Currently, I have enrolled in a PHP Laravel Development course. I have dream of building an ideal world where everythone loves each other. 💚";
+            echo "My name is $name.";
+            echo "<br>";
+            echo "I am $age years old.";
+            echo "<br>";
+            echo "My country name is $country.";
+            echo "<br>";
+            echo "About Myself: $about_myself";
+        ?>
+    </body>
+</html>
---
+++ Temperature Converter/temperature_converter.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <head>
+        <meta charset = "UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Temperature Converter</title>
+
+        <style>
+            <?php include "styles.css" ?>
+        </style>
+    </head>
+    </head>
+    <body>
+    <div class = "container">
+            <h2>Temperature Converter</h2>
+            <form method = "post" action = "">
+
+                <select name = "Operation">
+                    <option value = "celsius">Celsius to Fahrenheit</option>
+                    <option value = "fahrenheit">Fahrenheit to Celsius</option>
+                </select><br>
+
+                <input type = "number" name = "temp" placeholder = "Temperature" required><br>
+
+                <button type = "submit">Calculate</button>
+            </form>
+
+            <div id ="result">
+                <?php
+                    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                        $number = $_POST["temp"];
+                        $operation = $_POST["Operation"];
+
+                        switch($operation) {
+                            case  "celsius":
+                                $result = ($number  * (9/5)) + 32;
+                                $formatted = round($result, 2);
+                                echo "$number&deg Celsius = $formatted&deg Fahrenheit";
+                                break;
+                            case  "fahrenheit":
+                                $result = ($number - 32) * 5 / 9;
+                                $formatted = round($result, 2);
+                                echo "$number&deg Fahrenheit = $formatted&deg Celsius";
+                                break;
+                        }
+                    }
+                ?>
+            </div>
+        </div>
+    </body>
+</html>
